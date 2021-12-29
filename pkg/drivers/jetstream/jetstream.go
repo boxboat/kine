2c12ccaa0e613954e69363c66c08cca644063a88
Matthew DeVenny <matt@boxboat.com>
2021-12-29 12:02:20 -0700

    test
    Signed-off-by: Matthew DeVenny <matt@boxboat.com>

--- pkg/drivers/jetstream/jetstream.go
+++ pkg/drivers/jetstream/jetstream.go
@@ -470,45 +470,39 @@ func (j *Jetstream) List(ctx context.Context, prefix, startKey string, limit, re
 	}
 }
 
-func (j *Jetstream) list(ctx context.Context, prefix, startKey string, limit, revision int64) (revRet int64, eventRet []*server.Event, errRet error) {
-	logrus.Tracef("list %s, start=%s, limit=%d, rev=%d", prefix, startKey, limit, revision)
-
-	keys, err := j.getKeys(ctx, prefix)
-
-	if err != nil {
-		return 0, nil, err
-	}
-
-	rev, err := j.currentRevision()
-	if err != nil {
-		return 0, nil, err
-	}
-
-	if revision == 0 && len(keys) == 0 {
-		return rev, nil, nil
-	} else if revision != 0 {
-		rev = revision
-	}
-	var count int64 = 0
-	events := make([]*server.Event, 0)
-	for _, key := range keys {
-		//if strings.HasPrefix(key, prefix) {
-		if count < limit || limit == 0 {
-			if key >= startKey {
-				if _, entry, err := j.get(ctx, key, 0, false); err == nil && entry != nil {
-					//if !entry.Delete {
-					events = append(events, entry)
-					//}
-					count++
-				}
-			}
-		} else {
-			break
-		}
-		//}
-	}
-	return rev, events, nil
-}
+//func (j *Jetstream) list(ctx context.Context, prefix, startKey string, limit, revision int64) (revRet int64, eventRet []*server.Event, errRet error) {
+//	logrus.Tracef("list %s, start=%s, limit=%d, rev=%d", prefix, startKey, limit, revision)
+//
+//	keys, err := j.getKeys(ctx, prefix)
+//
+//	if err != nil {
+//		return 0, nil, err
+//	}
+//
+//	rev, err := j.currentRevision()
+//	if err != nil {
+//		return 0, nil, err
+//	}
+//
+//	if revision == 0 && len(keys) == 0 {
+//		return rev, nil, nil
+//	} else if revision != 0 {
+//		rev = revision
+//	}
+//	var count int64 = 0
+//	events := make([]*server.Event, 0)
+//	for _, key := range keys {
+//		if count < limit || limit == 0 {
+//			if _, entry, err := j.get(ctx, key, 0, false); err == nil && entry != nil {
+//				events = append(events, entry)
+//				count++
+//			}
+//		} else {
+//			break
+//		}
+//	}
+//	return rev, events, nil
+//}
 
 // Count returns an exact count of the number of matching keys and the current revision of the database
 func (j *Jetstream) Count(ctx context.Context, prefix string) (revRet int64, count int64, err error) {
@@ -599,24 +593,29 @@ func (j *Jetstream) Update(ctx context.Context, key string, value []byte, revisi
 
 }
 
-func (j *Jetstream) Watch(ctx context.Context, key string, revision int64) <-chan []*server.Event {
-	logrus.Tracef("WATCH %s, rev=%d", key, revision)
+func (j *Jetstream) Watch(ctx context.Context, prefix string, revision int64) <-chan []*server.Event {
+	logrus.Tracef("WATCH %s, rev=%d", prefix, revision)
 
-	_, events, err := j.list(ctx, key, "", 0, 0)
+	//keys, err := j.getKeys(ctx, prefix)
+	//if err != nil {
+	//	logrus.Errorf("failed to get keys %s", prefix)
+	//}
 
-	watcher, err := j.kvBucket.Watch(key, nats.IgnoreDeletes())
+	//_, events, err := j.list(ctx, prefix, "", 0, 0)
+
+	watcher, err := j.kvBucket.Watch(prefix, nats.IgnoreDeletes())
 
 	if err != nil {
-		logrus.Errorf("failed to create watcher %s for revision %d", key, revision)
+		logrus.Errorf("failed to create watcher %s for revision %d", prefix, revision)
 	}
 
 	result := make(chan []*server.Event, 100)
 
 	go func() {
 
-		if len(events) > 0 {
-			result <- events
-		}
+		//if len(events) > 0 {
+		//	result <- events
+		//}
 
 		for {
 			select {
@@ -637,9 +636,9 @@ func (j *Jetstream) Watch(ctx context.Context, key string, revision int64) <-cha
 				}
 			case <-ctx.Done():
 				if err := watcher.Stop(); err != nil {
-					logrus.Warnf("error stopping %s watcher: %v", key, err)
+					logrus.Warnf("error stopping %s watcher: %v", prefix, err)
 				} else {
-					logrus.Debugf("stopped %s watcher", key)
+					logrus.Debugf("stopped %s watcher", prefix)
 				}
 				return
 			}
